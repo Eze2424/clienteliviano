fe8be3cf13adcd0c48e28042c838ef658967dfb8
Eze2424 <ezponce@frba.utn.edu.ar>
2026-09-18 11:44:36 -0300

    Initial commit

---
+++ src/test/java/ar/edu/utn/frba/ddsi/clienteliviano/ClientelivianoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.ddsi.clienteliviano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientelivianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
